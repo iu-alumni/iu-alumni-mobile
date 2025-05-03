0009c740712b74b00321cc4cfa1805571f89b244
Kamil Foatov <xdkomel@gmail.com>
2025-05-03 17:05:41 +0300

    code refactoer

--- lib/presentation/pages/event/widgets/participants_card.dart
+++ lib/presentation/pages/event/widgets/participants_card.dart
@@ -1,7 +1,5 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fpdart/fpdart.dart';
 
 import '../../../blocs/models/one_event_state.dart';
 import '../../../blocs/one_event/one_event_cubit.dart';
@@ -10,7 +8,7 @@ import '../../../common/constants/app_text_styles.dart';
 import '../../../common/models/loaded_state.dart';
 import '../../../common/widgets/profile_pic.dart';
 import '../../../common/widgets/titled_item.dart';
-import '../../../router/app_router.gr.dart';
+import 'participants_modal.dart';
 
 class ParticipantsCard extends StatelessWidget {
   const ParticipantsCard({super.key});
@@ -20,95 +18,7 @@ class ParticipantsCard extends StatelessWidget {
         isScrollControlled: true,
         builder: (_) => BlocProvider.value(
           value: context.read<OneEventCubit>(),
-          child: Material(
-            color: Colors.white,
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(24),
-              topRight: Radius.circular(24),
-            ),
-            clipBehavior: Clip.antiAlias,
-            child: SingleChildScrollView(
-              child: BlocBuilder<OneEventCubit, OneEventState>(
-                buildWhen: (p, c) => p.participants != c.participants,
-                builder: (context, state) => switch (state.participants) {
-                  LoadedStateData(:final data) when data.isEmpty => Padding(
-                      padding: const EdgeInsets.all(24),
-                      child: Text(
-                        'An error occurred when loading participants',
-                        style: AppTextStyles.caption,
-                        textAlign: TextAlign.left,
-                      ),
-                    ),
-                  LoadedStateData(:final data) => Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        const SizedBox(height: 24),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 24),
-                          child: Text('Participants', style: AppTextStyles.h4),
-                        ),
-                        const SizedBox(height: 8),
-                        for (final p in data)
-                          InkWell(
-                            onTap: () => context.pushRoute(
-                              ProfileRoute(profile: Option.of(p)),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 8,
-                                horizontal: 24,
-                              ),
-                              child: Row(
-                                children: [
-                                  ProfilePic(profile: p, size: 48),
-                                  const SizedBox(width: 16),
-                                  Expanded(
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.stretch,
-                                      children: [
-                                        Text(
-                                          '${p.firstName} ${p.lastName}',
-                                          style:
-                                              AppTextStyles.buttonText.copyWith(
-                                            color: AppColors.darkGray,
-                                          ),
-                                          textAlign: TextAlign.start,
-                                        ),
-                                        if (p.location
-                                            case final location?) ...[
-                                          const SizedBox(height: 2),
-                                          Text(
-                                            location,
-                                            style: AppTextStyles.h6.copyWith(
-                                              color: AppColors.darkGray
-                                                  .withValues(alpha: 0.5),
-                                            ),
-                                            textAlign: TextAlign.start,
-                                          ),
-                                        ]
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        const SizedBox(height: 24),
-                      ],
-                    ),
-                  _ => const Center(
-                      child: Padding(
-                        padding: EdgeInsets.all(24),
-                        child: CircularProgressIndicator(
-                          color: AppColors.blueGray,
-                        ),
-                      ),
-                    )
-                },
-              ),
-            ),
-          ),
+          child: const ParticipantsModal(),
         ),
       );
 
