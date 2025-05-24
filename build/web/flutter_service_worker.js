42d763af4a46f92bc100a49ea6a9a7edbcd9d28b
Kamil Foatov <xdkomel@gmail.com>
2025-05-24 18:43:20 +0300

    add back button support for tg mini app

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a71a9a5a1e826a7bd17ef20a2e93a3ae",
+const RESOURCES = {"flutter_bootstrap.js": "176d76d9c5dba2fc4f13426a8b2387e9",
 "version.json": "f281bbdb3b654f37040f14ebcfcdd35c",
-"index.html": "bb079f6c226a79bc005222432b7c3f7b",
-"/": "bb079f6c226a79bc005222432b7c3f7b",
+"index.html": "edfba17059a20ce0f47c8630d2f1fd08",
+"/": "edfba17059a20ce0f47c8630d2f1fd08",
 "main.dart.js": "9b1828b7617df86363ae24aed2567f09",
 "sqlite3.wasm": "fe0b5f7d20d0d900508725319188a9a8",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
